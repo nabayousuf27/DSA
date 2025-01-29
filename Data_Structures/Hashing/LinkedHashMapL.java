494b45798730c763eeeeb9355748647663a560f2
Naba Yousuf <nabay0519@gmail.com>
2025-01-29 16:31:05 +0500

    change in class name

---
+++ Data_Structures/Hashing/LinkedHashMapL.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class LinkedHashMapL{
+    public static void main(String[] args) {
+        LinkedHashMap<String , Integer> lhm = new LinkedHashMap<>();
+        lhm.put("US",80);
+        lhm.put("Pakistan" , 100);
+        lhm.put("China",50);
+
+        System.out.println(lhm);
+        
+    }
+}
