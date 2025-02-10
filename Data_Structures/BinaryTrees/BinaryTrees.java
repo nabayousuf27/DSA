c519afbfe9dd126fbbe0d4f7f5b1b07555d4bcd3
Naba Yousuf <nabay0519@gmail.com>
2025-02-10 11:49:59 +0500

    preorder traversal

--- Data_Structures/BinaryTrees/BinaryTrees.java
+++ Data_Structures/BinaryTrees/BinaryTrees.java
@@ -26,12 +26,21 @@ public class BinaryTrees {
             
             return newNode;
         }
+        public static void preOrder(Node root){ //0(n)
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+        tree.preOrder(root);
     }
     
 }
