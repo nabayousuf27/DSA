1bd8c7ae46622cc30857bdf1e12f14ddc04d319b
Naba Yousuf <nabay0519@gmail.com>
2025-02-11 18:32:32 +0500

    implement Binary Search Tree

---
+++ Data_Structures/BST/BST.java
@@ -0,0 +1,47 @@
+package Data_Structures.BST;
+
+public class BST {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.data > val){
+            //left subtree
+            root.left = insert(root.left, val);
+        } else {
+            //right subtree
+            root.right = insert(root.right,val);
+        }
+        return root;
+    }
+    //to print bst usnig inorder traversal
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }   
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+
+    }
+
+    public static void main(String[] args) {
+        int value[] = {5,1,3,4,2,7};
+        Node root  = null ;
+
+        for(int i = 0 ; i< value.length; i++){
+            root  = insert(root , value[i]);
+        }
+        inorder(root);
+        System.out.println();
+    }
+}
