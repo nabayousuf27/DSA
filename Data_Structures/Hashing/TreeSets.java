085ddd6d0a3ee10aa6707dd02fd469a358f58445
Naba Yousuf <nabay0519@gmail.com>
2025-02-11 19:16:34 +0500

    implementing tree sets

---
+++ Data_Structures/Hashing/TreeSets.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class TreeSets {
+    //sorted assecending order output 
+        public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("US");
+        ts.add("Pakistan");
+        ts.add("China");
+        System.out.println(ts);
+    }
+}
